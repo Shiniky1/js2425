c600199d2e21f986875a51cb2bbe41570f7062f5
Frontend JavaScript <frontendjavascript@iMAC-07.local>
2025-01-30 20:45:43 +0200

    curs9

---
+++ curs9/unittest/js/jasmine/src/math.js
@@ -0,0 +1,14 @@
+function helloWorld(){
+    return "Hello world";
+}
+function sum(a,b){
+    return(a+b);
+}
+
+function sum(a,b){
+    a=a*100;
+    b=b*100;
+    let sum = a+b;
+    sum=sum/100;
+    return Number(sum.toFixed(2));
+}
